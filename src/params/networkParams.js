f87edcc7aa9df0e096155d276ab7b5ff2e2a55de
Prabhjot <prabhjot.narula@gmail.com>
2021-03-05 09:36:14 -0400

    fix: network params changes

---
+++ src/params/networkParams.js
@@ -0,0 +1,13 @@
+export const testnetCheck = window.location.origin !== 'http://bitshares-ui-realdata.test.graphenelab.io';
+
+export const defaultToken = testnetCheck ? 'TEST' : 'TEST';
+export const defaultNetwork = testnetCheck ? 'PeerplaysTestnet' : 'Peerplays';
+export const defaultQuote = testnetCheck ? 'BTC' : 'BTC';
+export const faucetUrl = testnetCheck ? 'https://irona-faucet.peerplays.download/api/v1/accounts' : 'https://irona-faucet.peerplays.download/api/v1/accounts';
+export const defaultChainId = '4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8';
+
+export const defaultChainParams = {
+    core_asset: defaultToken,
+    chain_id: defaultChainId,
+    address_prefix: defaultToken
+};
