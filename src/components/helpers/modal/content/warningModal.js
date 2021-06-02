3d3c6d720845a0ab6df51bc6eab107f11cd6ca58
blockchain-jsdev <>
2021-06-02 20:38:20 +0530

    loading spinner in membership page

--- src/components/helpers/modal/content/warningModal.js
+++ src/components/helpers/modal/content/warningModal.js
@@ -5,38 +5,23 @@ import ModalButton from "../../buttons/modalButton";
 import Translate from "react-translate-component";
 import {clearLayout} from "../../../../dispatch/layoutDispatch";
 import {defaultTrx} from "../../../../actions/forms";
-import { css } from "@emotion/react";
-import MoonLoader from "react-spinners/MoonLoader";
-
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-`;
 
 class WarningModal extends Component{
 
     state = {
-        errors: this.props.error,
-        loading: false,
-        upgraded:false,
-        hideModal: false
+        errors: this.props.error
     };
 
     handleMethod = () => {
         const {trx, password} = this.props;
-        this.setState({loading:true,hideModal:true});
         defaultTrx({trx, password})
-            .then(result => {
-                if(result.success){
-                    this.setState({loading:false,upgraded:true});
-                        setTimeout(() => {
-                        clearLayout();
-                        window.location.reload();
-                    }, 3000);
-                }})
+            .then(result => { if(result.success){
+                // clearLayout();   
+                window.location.reload();
+            }
+        })
             .catch(err => {
-                if(err.message.includes('Insufficient Balance')) this.setState({errors: 'isNotEnough', loading:false,hideModal:false})
+                if(err.message.includes('Insufficient Balance')) this.setState({errors: 'isNotEnough'})
             });
     };
 
@@ -44,11 +29,9 @@ class WarningModal extends Component{
 
         const fee = this.props.fee;
         const errors = this.state.errors;
-        const {loading,upgraded,hideModal} = this.state;
+
         return(
             <Fragment>
-                {!hideModal && 
-                <div>
                 <ModalTitle tag="warning" />
                 <Translate
                     component="div"
@@ -60,15 +43,6 @@ class WarningModal extends Component{
                     <Close />
                     {!errors && <ModalButton tag="continue" onClick={this.handleMethod} /> }
                 </div>
-                </div>
-                }
-                
-                {loading && 
-                <div className="sweet-loading">
-                    <MoonLoader css={override} size={50} color={"#123abc"} loading={this.state.loading} speedMultiplier={1} />
-                </div>}
-                
-                {upgraded && <span className="clr--positive">Successfully upgraded to the lifetime subscription.</span>}
             </Fragment>
         )
     }
