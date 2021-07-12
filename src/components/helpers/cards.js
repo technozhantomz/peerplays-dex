813d26a3d2001bdcc9749dc89c851dc1f5e79509
blockchain-jsdev <>
2021-07-12 12:25:53 +0530

    Business page is not responsive

--- src/components/helpers/cards.js
+++ src/components/helpers/cards.js
@@ -6,6 +6,7 @@ import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import Translate from "react-translate-component";
 import Grid from '@material-ui/core/Grid';
+import Link from "react-router-dom/es/Link";
 
 const useStyles = makeStyles({
   paper: {
@@ -16,14 +17,14 @@ const useStyles = makeStyles({
     textAlign: 'left',
     marginLeft: '40px'
   },
-  tabledata:{
+  tabledata: {
     fontSize: 14,
     textAlign: 'left'
   },
-  new:{
-    borderRadius:'10px',
-    border:'ridge',
-    marginTop:'2px'
+  new: {
+    borderRadius: '10px',
+    border: 'ridge',
+    marginTop: '2px'
   }
 });
 
@@ -33,33 +34,49 @@ export default function TableCard({ className, tableHead, rows, link, onClick, p
   return (
     <Card className="cardsTable">
       {rows.map((trItem, trId) => (
-      <Grid key={`tr-${trId}`} style={{borderRadius:'10px',border:'ridge',marginTop:'2px'}}>
-        {tableHead.map((tdItem, tdId) => (
-        <Grid container className={classes.paper} key={`td-${tdId}`}>
-          <Grid item xs={6} sm={6}>
-              <Box component="div" className={classes.tablehead} fontWeight="fontWeightBold">
-              {tdItem.translateTag
-                        ? <Translate
-                            key={`th-${tdId}`}
-                            content={`tableHead.${tdItem.translateTag}`}
-                            component="div"
-                            with={tdItem.translateParams}
-                        />
-                        : <div
-                            key={`th-${tdId}`}
-                            className={`tablecell ${tdItem.params ? tdItem.params : ''}`}
-                />}  
-              </Box>
-          </Grid>
-          <Grid item xs={6} sm={6} className="table__row">
-              <Typography component="div" className={classes.tabledata} key={`td-${tdId}`}>
-                {trItem[tdItem.key]}
-              </Typography>
-          </Grid>
-          <Divider component="h1" />
+        <Grid key={`tr-${trId}`} style={{ borderRadius: '10px', border: 'ridge', marginTop: '2px' }}>
+          {tableHead.map((tdItem, tdId) => (
+            <Grid container className={classes.paper} key={`td-${tdId}`}>
+              <Grid item xs={6} sm={6}>
+                <Box component="div" className={classes.tablehead} fontWeight="fontWeightBold">
+                  {tdItem.translateTag
+                    ? <Translate
+                      key={`th-${tdId}`}
+                      content={`tableHead.${tdItem.translateTag}`}
+                      component="div"
+                      with={tdItem.translateParams}
+                    />
+                    : <div
+                      key={`th-${tdId}`}
+                      className={`table__cell ${tdItem.params ? tdItem.params : ''}`}
+                    />}
+                </Box>
+              </Grid>
+              <Grid item xs={6} sm={6} className="table__row">
+                <Typography component="div" className={classes.tabledata} key={`td-${tdId}`}>
+                  {trItem[tdItem.key]}
+                </Typography>
+              </Grid>
+
+            </Grid>
+
+          ))}
+          {partialFill
+            && <div
+              className="table__partialFill"
+              style={{
+                backgroundColor: partialFill.color,
+                width: `${trItem[partialFill.percentKey]}%`
+              }}
+            />
+          }
+          {link
+            && <Link to={`${link.path}${trItem[link.key]}`} className="table__link" />
+          }
+          {onClick
+            && <button onClick={() => onClick(trItem)} className="table__link" />}
         </Grid>
-        ))}
-      </Grid>
+
       ))}
     </Card>
   );
