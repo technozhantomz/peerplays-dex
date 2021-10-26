562fb88715e0e2cd63dadc62ef6ed89a39c1f512
blockchain-jsdev <>
2021-10-26 06:57:22 -0300

    #491 To add confirmation message whether the sidechain address has been generated

--- src/components/helpers/modal/content/generateAddress.js
+++ src/components/helpers/modal/content/generateAddress.js
@@ -7,16 +7,19 @@ import Submit from "../decoration/submit";
 import ModalTitle from "../decoration/modalTitle";
 import { setSidechainAccounts } from '../../../../dispatch/setAccount';
 import { removeModal } from '../../../../dispatch/setModal';
+import {clearLayout} from "../../../../dispatch/index";
+// const handleAddressGenerated = ({sidechainAccounts}) => {
+//     console.log(sidechainAccounts);
+//     if(sidechainAccounts) {
+//         setSidechainAccounts(sidechainAccounts);
+//     }
 
-const handleAddressGenerated = ({sidechainAccounts}) => {
-    if(sidechainAccounts) {
-        setSidechainAccounts(sidechainAccounts);
-    }
+//     removeModal();
+// }
 
-    removeModal();
-}
 class GenerateAddress extends Component {
   state = {
+    sended: false,
     defaultData: {
       sidechain: this.props.sidechain,
       depositPublicKey: '',
@@ -27,8 +30,17 @@ class GenerateAddress extends Component {
     }
   };
 
+  handleAddressGenerated = (data) => {
+    this.setState({sended: true}, () => {
+    });
+
+    setTimeout(() => {
+        clearLayout()
+    }, 5000);
+};
+
   render() {
-    const {defaultData} = this.state;
+    const {defaultData, sended} = this.state;
     const {sidechain} = this.props;
 
     return (
@@ -39,10 +51,13 @@ class GenerateAddress extends Component {
                 requiredFields={['depositPublicKey', 'withdrawPublicKey', 'withdrawAddress']}
                 defaultData={defaultData}
                 action={generateSidechainAddress}
-                handleResult={handleAddressGenerated}
+                handleResult={this.handleAddressGenerated}
             >
                 {
-                    form => <Fragment>
+                    form => {
+                        const {errors} = form.state;
+                        return (
+                    <Fragment>
                         <div className="modal__content">
                             <Input
                                 name="depositPublicKey"
@@ -71,11 +86,15 @@ class GenerateAddress extends Component {
                                 Fee: {form.state.data.fee.amount} {form.state.data.fee.symbol}
                             </div>
                         </div>
+                        {sended && <span className="clr--positive">Sidechain address has been generated.</span>}
+                        {errors === "ERROR" && <h3 className="clr--negative">Something went wrong!! Try again.</h3>}
                         <div className="modal__bottom">
                             <Close />
                             <Submit tag="create" />
                         </div>
                     </Fragment>
+                        )
+                    }
                 }
             </Form>
         </Fragment>
