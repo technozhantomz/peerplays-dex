c5b323b003d859aab7694bb9eec850a390e2a32c
Furqan Aziz <faziz@peerplays.com>
2021-11-16 18:47:21 +0500

    #388 No result found displayed when user search in Users, Blocks, Token

--- src/actions/forms/globalSearch/lookupAccounts.js
+++ src/actions/forms/globalSearch/lookupAccounts.js
@@ -4,16 +4,22 @@ import {ContactItem} from "../../../components/helpers/contactItem";
 import {defaultToken} from "../../../params/networkParams";
 import {dbApi} from "../../nodes";
 
-const setAccountItem = (data) => <ContactItem key={data.id} data={data} handleClick={clearLayout} />;
+const setAccountItem = (data) => <ContactItem key={data.id} data={data} handleClick={clearLayout}/>;
 
-export const lookupAccounts = val => {
-    if(val.indexOf(defaultToken) === 0 && val.length === 54) return dbApi('get_key_references', [[val]])
+export const lookupAccounts = async val => {
+    if (val.indexOf(defaultToken) === 0 && val.length === 54) return dbApi('get_key_references', [[val]])
         .then(result => result[0]
             ? dbApi('get_accounts', [[result[0][0]]])
                 .then(userList => userList.map(({name, id}) => setAccountItem({name, id}))).catch(() => [])
             : []
         );
-
-    return dbApi('lookup_accounts', [val, 20])
+    let result;
+    result = await dbApi('lookup_accounts', [val, 20])
         .then(result => result.filter(e => e[0].includes(val)).map(([name, id]) => setAccountItem({name, id})))
+
+    if (result.length > 0) {
+        return result;
+    } else {
+        return 'No result Found';
+    }
 };
