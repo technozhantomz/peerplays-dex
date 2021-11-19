1c54dc5b09890c308d5dad01feb0fd5228924493
Furqan Aziz <faziz@peerplays.com>
2021-11-19 10:03:39 +0000

    bug #388 'No result Found' message is not displayed when the searched User, Blocks and Tokens are not found at Search page.

--- src/actions/forms/globalSearch/lookupAccounts.js
+++ src/actions/forms/globalSearch/lookupAccounts.js
@@ -6,14 +6,16 @@ import {dbApi} from "../../nodes";
 
 const setAccountItem = (data) => <ContactItem key={data.id} data={data} handleClick={clearLayout} />;
 
-export const lookupAccounts = val => {
+export const lookupAccounts = async val => {
     if(val.indexOf(defaultToken) === 0 && val.length === 54) return dbApi('get_key_references', [[val]])
         .then(result => result[0]
             ? dbApi('get_accounts', [[result[0][0]]])
                 .then(userList => userList.map(({name, id}) => setAccountItem({name, id}))).catch(() => [])
             : []
         );
-
-    return dbApi('lookup_accounts', [val, 20])
+    let result = await dbApi('lookup_accounts', [val, 20])
         .then(result => result.filter(e => e[0].includes(val)).map(([name, id]) => setAccountItem({name, id})))
+    if (result.length > 0)
+        return result;
+    return 'No result Found';
 };
