d50390bd66fb5ae2add97aa959c51dea5136d8f0
Furqan Aziz <faziz@peerplays.com>
2021-11-30 13:34:44 +0500

    #389 set default value for block to

--- src/actions/forms/globalSearch/lookupBlock.js
+++ src/actions/forms/globalSearch/lookupBlock.js
@@ -4,7 +4,7 @@ import {clearLayout} from "../../../dispatch/layoutDispatch";
 import {dbApi} from "../../nodes";
 import BlockHeader from "../../../components/helpers/blockHeader";
 
-export const lookupBlock = val => dbApi('get_blocks', [val,(parseInt( val ) + parseInt(process.env.BLOCK_TO_VALUE)).toString()]).then(async info => {
+export const lookupBlock = val => dbApi('get_blocks', [val,(parseInt( val ) + parseInt(process.env.BLOCK_TO_VALUE ? process.env.BLOCK_TO_VALUE : 100)).toString()]).then(async info => {
     if(!info) return 'No result Found';
     return (
         <span className="global-search__card card grid__cade">
