e71d4714bdc5a4f9839194ee6f579150fba8cd38
Furqan Aziz <faziz@peerplays.com>
2021-12-01 11:51:19 +0500

    #389 now you can serach with block number

--- src/actions/forms/globalSearch/lookupBlock.js
+++ src/actions/forms/globalSearch/lookupBlock.js
@@ -6,11 +6,13 @@ import BlockHeader from "../../../components/helpers/blockHeader";
 // Configs
 export const BLOCK_TO_VALUE = 100;
 
-export const lookupBlock = val => dbApi('get_blocks', [val,(parseInt( val ) + BLOCK_TO_VALUE).toString()]).then(async info => {
-    if(!info) return 'No result Found';
-    return (
-        <span className="global-search__card card grid__cade">
-            <BlockHeader num={val} data={info} />
-        </span>
-    )
-});
+export const lookupBlock = async val => {
+    return +val ? dbApi('get_blocks', [val, (parseInt(val) + BLOCK_TO_VALUE).toString()]).then(info => {
+        if (!info) return 'No result Found';
+        return (
+            <span className="global-search__card card grid__cade">
+                    <BlockHeader num={val} data={info}/>
+                </span>
+        )
+    }) : 'You can only serach with block number'
+};
