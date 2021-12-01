61a3e3c62c7da2409d273734c635cec12b821c51
Furqan Aziz <faziz@peerplays.com>
2021-12-01 13:07:41 +0500

    #239 validation function is inprogress

--- src/components/pages/bitcoin/generateAddress.js
+++ src/components/pages/bitcoin/generateAddress.js
@@ -5,14 +5,16 @@ import {setSidechainAccounts} from '../../../dispatch/setAccount';
 import {clearLayout} from "../../../dispatch/index";
 import { getPassword } from "../../../actions/forms";
 import { useFormInput } from "./formInput";
-
-
+import { validate, getAddressInfo } from 'bitcoin-address-validation';
 
 const GenerateAddress = (props) => {
     const {loginData, accountData, sidechain} = props;
     const depositPublicKey = useFormInput('');
     const withdrawPublicKey = useFormInput('');
     const withdrawAddress = useFormInput('');
+    const [depositPublicKeyError, setDepositPublicKeyError] = useState({});
+    const [withdrawPublicKeyError, setWithdrawPublicKeyError] = useState({});
+    const [withdrawAddressError, setWithdrawAddressError] = useState({});
     const [sent, setSent] = useState(false);
     const [fee, setFee] = useState({amount: 0, symbol: accountData.assets[0].symbol});
     const [errors, setErrors] = useState('');    
@@ -34,6 +36,7 @@ const GenerateAddress = (props) => {
     };
 
     const submitGenerateAddress = () => {
+        const isValid = formValidation();
         getPassword(password => generateSidechainAddress({
             sidechain: sidechain,
             depositPublicKey: depositPublicKey.value,
@@ -45,6 +48,37 @@ const GenerateAddress = (props) => {
             result.success ? handleAddressGenerated(result.callbackData) : setErrors(result.errors);
         }));
     };
+
+    const formValidation = () => {
+        const depositPublicKeyError = {};
+        const withdrawPublicKeyError = {};
+        const withdrawAddressError = {};
+        let isValid = true;
+        if(depositPublicKey.value.trim.length == 0){
+            depositPublicKeyError.depositPublicKeyRequired = 'This field is required';
+            isValid = false;
+        }
+        if(!validate(depositPublicKey.value)){
+            depositPublicKeyError.depositPublicKeyValid = 'Deposit Public key not correct';
+            isValid = false;
+        }
+        if(depositPublicKey.value.trim.length == 0){
+            withdrawPublicKeyError.withdrawPublicKeyRequired = 'This field is required';
+            isValid = false;
+        }
+        if(!validate(depositPublicKey.value)){
+            withdrawPublicKeyError.withdrawPublicKeyValid = 'Deposit Public key not correct';
+            isValid = false;
+        }
+        if(depositPublicKey.value.trim.length == 0){
+            withdrawAddressError.depositPublicKeyRequired = 'This field is required';
+            isValid = false;
+        }
+        if(!validate(depositPublicKey.value)){
+            withdrawAddressError.depositPublicKeyValid = 'Deposit Public key not correct';
+            isValid = false;
+        }
+    }
 
     return(
         <div className="card__content">
