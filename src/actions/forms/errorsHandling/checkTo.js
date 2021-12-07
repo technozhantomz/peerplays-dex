c82e053ee09e9d4e654ef2722649073c549084cc
Furqan Aziz <faziz@peerplays.com>
2021-12-07 16:46:09 +0500

    #588 form already have required validation wo we not need to repeat in validator

--- src/actions/forms/errorsHandling/checkTo.js
+++ src/actions/forms/errorsHandling/checkTo.js
@@ -1,13 +1,10 @@
 import { dbApi } from "../../nodes";
 
 export const checkTo = ({ to, from }) => {
-
-  if (!to) return 'required';
-
   if (to && to === from)
     return 'sameAccount';
-
-  return dbApi('get_account_by_name', [to])
-    .then(e => !e ? 'accountIsNotExist' : false)
-    .catch(() => 'accountIsNotExist');
+  if(to && to.length > 0)
+    return dbApi('get_account_by_name', [to])
+        .then(e => !e ? 'accountIsNotExist' : false)
+        .catch(() => 'accountIsNotExist');
 }
