6e26bb3d78b25a5f8c3d460c94e10d95371123f0
Furqan Aziz <faziz@peerplays.com>
2021-12-10 16:38:58 +0500

    #580 In to validator past old code and remove field value that not have value

--- src/actions/forms/errorsHandling/checkTo.js
+++ src/actions/forms/errorsHandling/checkTo.js
@@ -1,10 +1,13 @@
 import { dbApi } from "../../nodes";
 
 export const checkTo = ({ to, from }) => {
+
+  if (!to) return 'required';
+
   if (to && to === from)
     return 'sameAccount';
-  if(to && to.length > 0)
-    return dbApi('get_account_by_name', [to])
-        .then(e => !e ? 'accountIsNotExist' : false)
-        .catch(() => 'accountIsNotExist');
+
+  return dbApi('get_account_by_name', [to])
+      .then(e => !e ? 'accountIsNotExist' : false)
+      .catch(() => 'accountIsNotExist');
 }
