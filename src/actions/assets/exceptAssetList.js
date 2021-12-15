20aba60be4e7bb5340a603d10d8f2d430111bd2f
blockchain-jsdev <>
2021-12-15 03:51:42 -0400

    #613 filter and remove junk asset names

---
+++ src/actions/assets/exceptAssetList.js
@@ -0,0 +1,13 @@
+const exceptAssetList = 
+    [
+        "AQSQFHZAJVZDTVAC",
+        "AXFMUJYATHGJSOIV",
+        "BSEBEIBGLWDPKMDB",
+        "CFQBVAIKFFJEEOOJ",
+        "DOQIBMVBUJYYDXXF",
+        "MCLUSD",
+        "BTFUN",
+        "SEUSD"
+    ];
+
+export default exceptAssetList;
