bc1fd944e9d97716b66f6c69ad3b78eced6bdad9
Alexis Tanaka <atanaka@peerplays.com>
2022-04-25 18:45:49 -0700

    precision-validation-added

--- src/actions/forms/errorsHandling/checkBalance.js
+++ src/actions/forms/errorsHandling/checkBalance.js
@@ -1,7 +1,8 @@
 import {getAccountData} from "../../store";
+import {getAssetBySymbol} from "../../../actions/assets"
 
 export const checkAmountToSell = ({type, buyAsset, sellAsset, amount_to_receive, amount_to_sell}) => {
-    if(!amount_to_sell) {
+  if(!amount_to_sell) {
       return 'required';
     } else {
       if(isNaN(amount_to_sell)){
@@ -25,8 +26,7 @@ export const checkAmountToSell = ({type, buyAsset, sellAsset, amount_to_receive,
     }
 }
 
-export const checkAmountToReceive = ({type, buyAsset, sellAsset, amount_to_sell, amount_to_receive}) => {
-
+export const checkAmountToReceive = async ({type, buyAsset, sellAsset, amount_to_sell, amount_to_receive}) => {
   if(!amount_to_receive) {
     return 'required';
   } else {
@@ -40,18 +40,43 @@ export const checkAmountToReceive = ({type, buyAsset, sellAsset, amount_to_sell,
       } else {
         return userAsset.setPrecision() >= amount_to_receive ? false : 'isNotEnough';
       }
-    } 
+    }
+
+    if (amount_to_receive >= 10 ** 8) {
+      return 'outofRangeOrBadPrecision'
+    }
+
+    const { precision } = await getAssetBySymbol(type === 'buy' ? buyAsset : sellAsset)
+    const amtStr = amount_to_receive.toString()
+    const fractionLength = amtStr.indexOf('.') === -1
+      ? 0
+      : (amtStr.length - amtStr.indexOf('.') - 1)
+    if (fractionLength > precision) {
+      return 'outofRangeOrBadPrecision'
+    }
+
     return false;
   }
 }
 
-export const checkPrice = ({price}) => {
+export const checkPrice = async ({price, sellAsset}) => {
   if(!price) {
     return 'required';
   } else {
     if(isNaN(price)){
       return 'isNan';
     } 
-    return false;
+    
+    if(price >= 10 ** 8) {
+      return 'outofRangeOrBadPrecision'
+    }
+    const { precision } = await getAssetBySymbol(sellAsset)
+    const priceStr = price.toString()
+    const fractionLength = priceStr.indexOf('.') === -1 
+                            ? 0 
+                            : (priceStr.length - priceStr.indexOf('.') - 1)
+    if (fractionLength > precision) {
+      return 'outofRangeOrBadPrecision'
+    }
   }
 }
