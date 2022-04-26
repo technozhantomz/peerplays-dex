b22ea30c5f7e56a2faec4bed5085a57bc17c4922
Alexis Tanaka <atanaka@peerplays.com>
2022-04-26 07:54:08 -0700

    precision-handling-fixed

--- src/actions/forms/errorsHandling/checkBalance.js
+++ src/actions/forms/errorsHandling/checkBalance.js
@@ -1,5 +1,4 @@
 import {getAccountData} from "../../store";
-import {getAssetBySymbol} from "../../../actions/assets"
 
 export const checkAmountToSell = ({type, buyAsset, sellAsset, amount_to_receive, amount_to_sell}) => {
   if(!amount_to_sell) {
@@ -41,42 +40,21 @@ export const checkAmountToReceive = async ({type, buyAsset, sellAsset, amount_to
         return userAsset.setPrecision() >= amount_to_receive ? false : 'isNotEnough';
       }
     }
-
-    if (amount_to_receive >= 10 ** 8) {
-      return 'outofRangeOrBadPrecision'
-    }
-
-    const { precision } = await getAssetBySymbol(type === 'buy' ? buyAsset : sellAsset)
-    const amtStr = amount_to_receive.toString()
-    const fractionLength = amtStr.indexOf('.') === -1
-      ? 0
-      : (amtStr.length - amtStr.indexOf('.') - 1)
-    if (fractionLength > precision) {
-      return 'outofRangeOrBadPrecision'
-    }
-
+    if(amount_to_receive >= 10 ** 8)
+      return 'isTooBig'
     return false;
   }
 }
 
-export const checkPrice = async ({price, sellAsset}) => {
+export const checkPrice = ({price}) => {
   if(!price) {
     return 'required';
   } else {
     if(isNaN(price)){
       return 'isNan';
-    } 
-    
-    if(price >= 10 ** 8) {
-      return 'outofRangeOrBadPrecision'
     }
-    const { precision } = await getAssetBySymbol(sellAsset)
-    const priceStr = price.toString()
-    const fractionLength = priceStr.indexOf('.') === -1 
-                            ? 0 
-                            : (priceStr.length - priceStr.indexOf('.') - 1)
-    if (fractionLength > precision) {
-      return 'outofRangeOrBadPrecision'
-    }
+    if (price >= 10 ** 8)
+      return 'isTooBig'
+    return false;
   }
 }
--- src/translations/en.js
+++ src/translations/en.js
@@ -689,7 +689,7 @@ export const en = {
         "required": "This field is required.",
         "invalidKey":"Invalid key.",
         "requiredQuantity": "Quantity is required.",
-        "outofRangeOrBadPrecision": "Value is too big or precision is incorrect.",
+        "isTooBig": "Value is too big.",
         "isNan": "Amount is not a number.",
         "isNull": "You can't send a null.",
         "isEmptyBalance": "You don't have balance to pay fee.",
